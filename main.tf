f39de5dd03f43140307bd5b9b416bff54075aaf9
Shane Rich <shanerich@Shanes-MacBook-Pro.local>
2024-01-19 13:48:06 +0100

    Moved ec2 instances to own module, and used moved blocks to retain resources and update IDs.

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-module "vpc" {
+module "learn_vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.4"
 
@@ -29,43 +29,65 @@ module "vpc" {
   tags = var.vpc_tags
 }
 
-module "security_group" {
-  source = "./modules/security_group"
-  vpc_id = module.vpc.vpc_id
+module "web_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id = module.learn_vpc.vpc_id
+
+  use_name_prefix = false
+
+  name        = "terraform-learn-move-sg"
+  description = "Security Group managed by Terraform"
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+module "ec2_instance" {
+  source         = "./modules/compute"
+  security_group = module.web_security_group.security_group_id
+  public_subnets = module.learn_vpc.public_subnets
 }
 
-resource "aws_instance" "example" {
-  ami                         = data.aws_ami.ubuntu.id
-  subnet_id                   = module.vpc.public_subnets[0]
-  instance_type               = "t2.micro"
-  vpc_security_group_ids      = [module.security_group.security_group_id]
-  associate_public_ip_address = true
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-  tags = {
-    Name = "terraform-learn-move-ec2"
-  }
+moved {
+  from = module.security_group.aws_security_group.sg_8080
+  to   = module.web_security_group.aws_security_group.this[0]
 }
+
+moved {
+  from = module.security_group.aws_security_group_rule.ingress_rule
+  to   = module.web_security_group.aws_security_group_rule.ingress_with_cidr_blocks[0]
+}
+
+moved {
+  from = module.security_group.aws_security_group_rule.egress_rule
+  to   = module.web_security_group.aws_security_group_rule.egress_with_cidr_blocks[0]
+}
+
+moved {
+  from = aws_instance.example
+  to = module.ec2_instance.aws_instance.example
+}
+
+moved {
+  from = module.vpc
+  to = module.learn_vpc
+}
+
